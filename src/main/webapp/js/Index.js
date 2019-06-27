cc19c23e41eaf0e3afd2c8ada745da4eb3da7fa5
Ai_Sama <79479334@qq.com>
2019-06-27 19:18:34 +0800

    2019-6-27 19:31 commit

--- src/main/webapp/js/Index.js
+++ src/main/webapp/js/Index.js
@@ -160,7 +160,6 @@ $(document).ready(function () {
 function lookAnima() {
     var animaurl=prompt("请输入樱花动漫番剧页面链接(不懂请看首页公告)");
     if(animaurl.trim().length>0){
-        alert(animaurl);
         window.location.replace("/AnimaController/chooseAnimaNums?url="+animaurl);
     }
 }
